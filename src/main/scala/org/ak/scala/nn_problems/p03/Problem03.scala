d98112a381f9641eefeb073a89d73a5ae04e1ae1
Anton Korovin <korovin.anton@gmail.com>
2014-08-12 14:47:36 +0300

    Added solution for Problem03.

---
+++ src/main/scala/org/ak/scala/nn_problems/p03/Problem03.scala
@@ -0,0 +1,34 @@
+package org.ak.scala.nn_problems.p03
+
+import scala.annotation.tailrec
+
+/**
+ * @author antonk
+ * @since  8/12/14 - 2:16 PM
+ */
+object Problem03 {
+  // Find the Kth element of a list.
+  // By convention, the first element in the list is element 0.
+  // Example:
+  //  scala> nth(2, List(1, 1, 2, 3, 5, 8))
+  // res0: Int = 2
+
+
+  @tailrec
+  def nth[T](n: Int, list: List[T]): T = {
+    if (n < 0) throw new IndexOutOfBoundsException
+
+    list match {
+      case head :: _ if n == 0 =>
+        head
+
+      case _ :: tail =>
+        nth(
+          n - 1,
+          tail
+        )
+
+      case Nil => throw new IndexOutOfBoundsException
+    }
+  }
+}
---
+++ src/test/scala/org/ak/scala/nn_problems/p03/Problem03Test.scala
@@ -0,0 +1,80 @@
+package org.ak.scala.nn_problems.p03
+
+import org.scalacheck.Gen
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+ * @author antonk
+ * @since  8/12/14 - 2:19 PM
+ */
+class Problem03Test extends FunSuite
+with Matchers
+with GeneratorDrivenPropertyChecks {
+
+  test("example test") {
+    Problem03.nth(2, List(1, 1, 2, 3, 5, 8)) shouldEqual 2
+  }
+
+
+  test("find nth in normal list") {
+    forAll(
+      Gen.nonEmptyListOf(
+        Gen.chooseNum(1, Int.MaxValue)
+      )
+    ) {
+      list =>
+        forAll(Gen.chooseNum(0, list.size - 1)) {
+          index =>
+            Problem03.nth(index, list) shouldEqual list(index)
+        }
+    }
+  }
+
+
+  test("find nth in empty list") {
+    forAll(
+      Gen.oneOf(List.empty, Nil, List())
+    ) {
+      anEmptyList => {
+        forAll(
+          Gen.chooseNum(Int.MinValue, Int.MaxValue)
+        ) {
+          index => {
+            intercept[IndexOutOfBoundsException] {
+              Problem03.nth(index, anEmptyList)
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+  test("find nth (out-of-bound) in normal list") {
+    forAll(
+      Gen.nonEmptyListOf(
+        Gen.chooseNum(Int.MinValue, Int.MaxValue)
+      )
+    ) {
+      list => {
+
+        forAll(
+          Gen.chooseNum(
+            list.size,
+            Int.MaxValue
+          )
+        ) {
+          index => {
+
+            intercept[IndexOutOfBoundsException] {
+              Problem03.nth(index, list)
+            }
+
+          }
+        }
+
+      }
+    }
+  }
+}
