32c4044838da930e66ca37ed9814ee9fdd742690
Anton Korovin <korovin.anton@gmail.com>
2014-08-13 12:38:50 +0300

    Added Problem06.

---
+++ src/main/scala/org/ak/scala/nn_problems/p06/Problem06.scala
@@ -0,0 +1,17 @@
+package org.ak.scala.nn_problems.p06
+
+/**
+ * @author antonk
+ * @since  8/13/14 - 12:29 PM
+ */
+object Problem06 {
+  //  Find out whether a list is a palindrome.
+  //  Example:
+  //    scala> isPalindrome(List(1, 2, 3, 2, 1))
+  //  res0: Boolean = true
+
+
+  def isPalindrome[T](list: List[T]) = {
+    list == list.reverse
+  }
+}
---
+++ src/test/scala/org/ak/scala/nn_problems/p06/Problem06Test.scala
@@ -0,0 +1,40 @@
+package org.ak.scala.nn_problems.p06
+
+import org.ak.scala.nn_problems.GenUtil
+import org.scalacheck.Gen
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+ * @author antonk
+ * @since  8/13/14 - 12:32 PM
+ */
+class Problem06Test
+  extends FunSuite
+  with Matchers
+  with GeneratorDrivenPropertyChecks {
+
+
+  test("example test") {
+    assert(Problem06.isPalindrome(List(1, 2, 3, 2, 1)))
+  }
+
+
+  test("find out 'isPalindrome' on normal list") {
+    forAll(
+      Gen.nonEmptyListOf(
+        GenUtil.chooseAllOfInt()
+      )
+    ) {
+      list =>
+        val palindrome = list.reverse ::: list
+
+        assert(
+          Problem06.isPalindrome(
+            palindrome
+          )
+        )
+    }
+  }
+
+}
