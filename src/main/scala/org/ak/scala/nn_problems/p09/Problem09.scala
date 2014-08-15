316a9cfab2032edf3d035d518195b99708249d11
Anton Korovin <korovin.anton@gmail.com>
2014-08-15 11:40:02 +0300

    Added Problem09.

---
+++ src/main/scala/org/ak/scala/nn_problems/p09/Problem09.scala
@@ -0,0 +1,60 @@
+package org.ak.scala.nn_problems.p09
+
+import scala.annotation.tailrec
+
+/**
+ * @author antonk
+ * @since  8/15/14 - 11:04 AM
+ */
+object Problem09 {
+  //  P09 (**) Pack consecutive duplicates of list elements into sublists.
+  //    If a list contains repeated elements they should be placed in separate sublists.
+  //    Example:
+  //
+  //    scala> pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+  //  res0: List[List[Symbol]] = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+
+
+  def packTailRecursive[T](list: List[T]) = {
+    @tailrec
+    def packRec(
+      rest: List[T],
+      elements: List[T],
+      packed: List[List[T]]
+    ): List[List[T]] = {
+      rest match {
+        case head :: tail if elements.isEmpty =>
+          packRec(
+            tail,
+            List(head),
+            packed
+          )
+
+
+        case head :: tail if head != elements.head =>
+          packRec(
+            tail,
+            List(head),
+            elements :: packed
+          )
+
+
+        case head :: tail =>
+          packRec(
+            tail,
+            head :: elements,
+            packed
+          )
+
+
+        case Nil =>
+          if (elements.isEmpty) packed
+          else                  elements :: packed
+      }
+
+
+    }
+
+    packRec(list, Nil, Nil).reverse
+  }
+}
---
+++ src/test/scala/org/ak/scala/nn_problems/p09/Problem09Test.scala
@@ -0,0 +1,23 @@
+package org.ak.scala.nn_problems.p09
+
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+ * @author antonk
+ * @since  8/15/14 - 11:18 AM
+ */
+class Problem09Test
+  extends FunSuite
+          with Matchers
+          with GeneratorDrivenPropertyChecks {
+
+  test("example test") {
+    val srcList = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    val expList = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+
+    import org.ak.scala.nn_problems.p09.Problem09._
+
+    packTailRecursive(srcList) shouldEqual expList
+  }
+}
