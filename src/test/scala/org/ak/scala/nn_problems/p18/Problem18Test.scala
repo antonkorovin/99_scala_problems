d2a15acfc30d8cec39fb0b63b28c14555f17923f
Anton Korovin <korovin.anton@gmail.com>
2014-08-19 12:26:13 +0300

    Added tests for Problem18.

---
+++ src/test/scala/org/ak/scala/nn_problems/p18/Problem18Test.scala
@@ -0,0 +1,86 @@
+package org.ak.scala.nn_problems.p18
+
+import org.ak.scala.nn_problems.GenUtil
+import org.ak.scala.nn_problems.p18.Problem18._
+import org.scalacheck.Gen
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+ * @author antonk
+ * @since  8/18/14 - 2:23 PM
+ */
+class Problem18Test
+  extends FunSuite
+          with Matchers
+          with GeneratorDrivenPropertyChecks {
+
+
+  test("example test") {
+    val srcList = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
+
+    slice(
+      3,
+      7,
+      srcList
+    ) shouldEqual List('d, 'e, 'f, 'g)
+
+
+    check(
+      srcList,
+      3,
+      7
+    )
+  }
+
+
+
+  test("various lists") {
+    forAll(
+      Gen.nonEmptyListOf(
+        GenUtil.chooseAllOfInt()
+      )
+    ) {
+      list =>
+        forAll(
+          Gen.chooseNum(0, list.size)
+        ) {
+          from =>
+
+            forAll(
+              Gen.chooseNum(from, list.size)
+            ) {
+              until =>
+
+                check(
+                  list,
+                  from,
+                  until
+                )
+
+            }
+        }
+    }
+  }
+
+
+  private def check[T](
+    list: List[T],
+    from: Int,
+    until: Int
+  ) {
+    slice(
+      from,
+      until,
+      list
+    ) shouldEqual list.slice(from, until)
+
+
+    sliceUsingDropAndTake(
+      from,
+      until,
+      list
+    ) shouldEqual list.slice(from, until)
+
+  }
+}
