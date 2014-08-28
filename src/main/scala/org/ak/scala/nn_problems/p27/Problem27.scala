17710f68907675ca3633b84270631e3077cbe67e
Anton Korovin <korovin.anton@gmail.com>
2014-08-28 18:48:28 +0300

    Started Problem27.

---
+++ src/main/scala/org/ak/scala/nn_problems/p27/Problem27.scala
@@ -0,0 +1,29 @@
+package org.ak.scala.nn_problems.p27
+
+/**
+ * @author antonk
+ * @since  8/28/14 - 6:44 PM
+ */
+object Problem27 {
+  //  P27 (**) Group the elements of a set into disjoint subsets.
+  //  a) In how many ways can a group of 9 people work in 3 disjoint subgroups of 2, 3 and 4 persons?
+  //     Write a function that generates all the possibilities.
+  //    Example:
+  //
+  //    scala> group3(List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida"))
+  //  res0: List[List[List[String]]] = List(List(List(Aldo, Beat), List(Carla, David, Evi), List(Flip, Gary, Hugo, Ida)), ...
+  //  b) Generalize the above predicate in a way that we can specify a list of group sizes and the predicate will return a list of groups.
+  //
+  //  Example:
+  //
+  //    scala> group(List(2, 2, 5), List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida"))
+  //  res0: List[List[List[String]]] = List(List(List(Aldo, Beat), List(Carla, David), List(Evi, Flip, Gary, Hugo, Ida)), ...
+  //  Note that we do not want permutations of the group members; i.e. ((Aldo, Beat), ...) is the same solution as ((Beat, Aldo), ...). However, we make a difference between ((Aldo, Beat), (Carla, David), ...) and ((Carla, David), (Aldo, Beat), ...).
+  //
+  //  You may find more about this combinatorial problem in
+  //  a good book on discrete mathematics under the term "multinomial coefficients".
+  //
+
+
+
+}
---
+++ src/test/scala/org/ak/scala/nn_problems/p27/Problem27Test.scala
@@ -0,0 +1,17 @@
+package org.ak.scala.nn_problems.p27
+
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+ * @author antonk
+ * @since  8/28/14 - 6:46 PM
+ */
+class Problem27Test
+  extends FunSuite
+          with Matchers {
+
+  test("example") {
+    pending
+  }
+
+}
