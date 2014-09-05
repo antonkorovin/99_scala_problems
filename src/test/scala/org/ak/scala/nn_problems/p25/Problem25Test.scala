84648d1a44d656a5681a663e2c1968429a13a255
Anton Korovin <korovin.anton@gmail.com>
2014-09-05 05:26:26 +0300

    Updated test for Problem25.

--- src/test/scala/org/ak/scala/nn_problems/p25/Problem25Test.scala
+++ src/test/scala/org/ak/scala/nn_problems/p25/Problem25Test.scala
@@ -1,8 +1,9 @@
 package org.ak.scala.nn_problems.p25
 
-import org.ak.scala.nn_problems.p25.Problem25.randomPermute
+import org.ak.scala.nn_problems.p25.Problem25._
+import org.scalacheck.Gen
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
-import org.scalatest.{Matchers, FunSuite}
+import org.scalatest.{FunSuite, Matchers}
 
 /**
  * @author antonk
@@ -13,13 +14,39 @@ class Problem25Test
           with Matchers
           with GeneratorDrivenPropertyChecks {
 
+  type PermuteMethod = List[Char] => List[Char]
+
   test("example test") {
-    val srcList = List('a, 'b, 'c, 'd, 'e, 'f)
-    val resList = randomPermute(srcList)
 
-    println(resList.mkString)
+    val srcList = List[Char]('a', 'b', 'c', 'd', 'e', 'f')
 
-    resList should have size srcList.size
-    resList should not be equal(srcList)
+    methodsUnderTest.foreach {
+      permuteMethod: PermuteMethod =>
+        checkLists(
+          srcList,
+          permuteMethod(srcList)
+        )
+    }
+  }
+
+
+
+
+
+  private def checkLists[T](
+    expected: List[T],
+    actual: List[T]
+  ) {
+    actual should have size expected.size
+    actual should not be equal(expected)
+
+    actual.toSet shouldEqual expected.toSet
+  }
+
+
+  private def methodsUnderTest[T] = {
+    randomPermute[T] _ ::
+      randomPermuteUsingSort[T] _ ::
+      Nil
   }
 }
