df2205ffdddb34fea65a2a1a5c4cc3bd493e21f6
Anton Korovin <korovin.anton@gmail.com>
2014-09-17 23:48:59 +0300

    Added `empty` method to BinaryTree.

--- src/main/scala/org/ak/scala/nn_problems/p25/BinaryTree.scala
+++ src/main/scala/org/ak/scala/nn_problems/p25/BinaryTree.scala
@@ -5,15 +5,23 @@ package org.ak.scala.nn_problems.p25
  * @since  09/17/14 - 11:32 PM
  */
 
-sealed trait BinaryTree[+T]
+sealed trait BinaryTree[+T] {
+  def empty: Boolean
+}
 
 case class Node[T](
   value: T,
   left: BinaryTree[T],
   right: BinaryTree[T]
-) extends BinaryTree[T]
+) extends BinaryTree[T] {
+  override def empty = false
+}
 
 
-case class Leaf[T](value: T) extends BinaryTree[T]
+case class Leaf[T](value: T) extends BinaryTree[T] {
+  override def empty = false
+}
 
-case object EmptyLeaf extends BinaryTree[Nothing]
+case object EmptyLeaf extends BinaryTree[Nothing] {
+  override def empty = true
+}
