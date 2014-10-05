ea5e2a5ad89d8a85375d2d7bc60336d04e1de231
Anton Korovin <korovin.anton@gmail.com>
2014-10-05 09:40:23 +0300

    Basic implementation BinaryTree.add method.

--- src/main/scala/org/ak/scala/nn_problems/p25/BinaryTree.scala
+++ src/main/scala/org/ak/scala/nn_problems/p25/BinaryTree.scala
@@ -20,6 +20,8 @@ sealed trait BinaryTree[+T] {
   def full: Boolean
 
   def size: Int
+
+  def add[VT >: T](newValue: VT): BinaryTree[VT]
 }
 
 case class Node[T](
@@ -32,6 +34,27 @@ case class Node[T](
   override def full = left.full && right.full
 
   override def size = 1 + left.size + right.size
+
+  override def add[VT >: T](newValue: VT) = {
+    (left, right) match {
+      case (Leaf(_), _) =>
+        Node(
+          value,
+          left.add(newValue),
+          right
+        )
+
+      case (_, Leaf(_)) =>
+        Node(
+          value,
+          left,
+          right.add(newValue)
+        )
+
+      case (_, _) =>
+        throw new IllegalArgumentException()
+    }
+  }
 }
 
 
@@ -41,6 +64,10 @@ case class Leaf[T](value: T) extends BinaryTree[T] {
   override def full = true
 
   override def size = 1
+
+  override def add[VT >: T](newValue: VT) = {
+    Node(value, Leaf(newValue), EmptyLeaf)
+  }
 }
 
 case object EmptyLeaf extends BinaryTree[Nothing] {
@@ -49,4 +76,8 @@ case object EmptyLeaf extends BinaryTree[Nothing] {
   override def full = false
 
   override def size = 0
+
+  override def add[T](newValue: T) = {
+    Leaf(newValue)
+  }
 }
