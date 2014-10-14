8f5d8242d93451e7bf3fac62a4b578dbc8b9e1cb
Anton Korovin <korovin.anton@gmail.com>
2014-10-14 07:15:49 +0300

    Added pending tests for Problem27.

--- src/test/scala/org/ak/scala/nn_problems/p27/Problem27Test.scala
+++ src/test/scala/org/ak/scala/nn_problems/p27/Problem27Test.scala
@@ -10,8 +10,44 @@ class Problem27Test
   extends FunSuite
           with Matchers {
 
-  test("example") {
-    pending
+  test("group3") {
+    pendingUntilFixed {
+      // TODO Fill expected list correctly
+      val expectedList = List(
+        List(
+          List("Aldo", "Beat"),
+          List("Carla", "David", "Evi"),
+          List("Flip", "Gary", "Hugo", "Ida")
+        )
+      )
+
+
+      Problem27.group3(
+        List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida")
+      ) shouldEqual expectedList
+    }
   }
 
+
+  test("groupList") {
+    pendingUntilFixed {
+
+      // TODO Fill expected list correctly
+
+      val expectedList = List(
+        List(
+          List("Aldo", "Beat"),
+          List("Carla", "David", "Evi"),
+          List("Flip", "Gary", "Hugo", "Ida")
+        )
+      )
+
+
+      Problem27.group(
+        List(1, 3, 5),
+        List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida")
+      ) shouldEqual expectedList
+
+    }
+  }
 }
