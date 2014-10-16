af0eff07d323c48191a1b454cfb50b3bdc25ba89
Anton Korovin <korovin.anton@gmail.com>
2014-10-17 01:52:01 +0300

    Added pending test for Problem31.

---
+++ src/test/scala/org/ak/scala/nn_problems/p31/Problem31Test.scala
@@ -0,0 +1,20 @@
+package org.ak.scala.nn_problems.p31
+
+import org.ak.scala.nn_problems.arithmetic.S99Int._
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+ * @author antonk
+ * @since  10/17/14 - 1:47 AM
+ */
+class Problem31Test
+  extends FunSuite
+          with Matchers {
+
+  test("example") {
+    pendingUntilFixed {
+      7.isPrime shouldBe true
+    }
+  }
+
+}
