e5b84dc267a0d236e8a2e1fe7d5c8b7d31dacf59
Anton Korovin <korovin.anton@gmail.com>
2014-10-17 01:50:54 +0300

    Added stub for Problem31.

---
+++ src/main/scala/org/ak/scala/nn_problems/p31/Problem31.scala
@@ -0,0 +1,13 @@
+package org.ak.scala.nn_problems.p31
+
+/**
+ * @author antonk
+ * @since  10/17/14 - 1:45 AM
+ */
+object Problem31 {
+  //  P31 (**) Determine whether a given integer number is prime.
+  //    scala> 7.isPrime
+  //  res0: Boolean = true
+
+  def isPrime(i: Int): Boolean = false
+}
