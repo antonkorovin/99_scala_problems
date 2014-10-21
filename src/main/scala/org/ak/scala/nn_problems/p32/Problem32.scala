f5ba31db4bbfed8c5aa6e3642bed1ffc2cc8eb5c
Anton Korovin <korovin.anton@gmail.com>
2014-10-22 00:33:12 +0300

    Added stub for Problem32.

---
+++ src/main/scala/org/ak/scala/nn_problems/p32/Problem32.scala
@@ -0,0 +1,14 @@
+package org.ak.scala.nn_problems.p32
+
+/**
+ * @author antonk
+ * @since  10/22/14 - 12:32 AM
+ */
+object Problem32 {
+  //  P32 (**) Determine the greatest common divisor of two positive integer numbers.
+  //    Use Euclid's algorithm.
+  //  scala> gcd(36, 63)
+  //  res0: Int = 9
+
+  def gcd(a: Int, b: Int) = 0
+}
