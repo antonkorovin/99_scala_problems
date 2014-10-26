e9e1f4f019c5d20b253c95dc0bc6766d2c9578bc
Anton Korovin <korovin.anton@gmail.com>
2014-10-26 06:29:04 +0200

    Added pending test for Problem32.

---
+++ src/test/scala/org/ak/scala/nn_problems/p32/Problem32Test.scala
@@ -0,0 +1,19 @@
+package org.ak.scala.nn_problems.p32
+
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+ * @author antonk
+ * @since  10/26/14 - 6:26 AM
+ */
+class Problem32Test
+  extends FunSuite
+          with Matchers {
+
+  test("example") {
+    pendingUntilFixed {
+      Problem32.gcd(36, 63) shouldEqual 9
+    }
+  }
+
+}
