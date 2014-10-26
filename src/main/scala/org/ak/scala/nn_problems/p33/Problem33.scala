997a63fa33f215e6b2204644225863b0749f50dc
Anton Korovin <korovin.anton@gmail.com>
2014-10-26 06:35:02 +0200

    Added stub for Problem33.

---
+++ src/main/scala/org/ak/scala/nn_problems/p33/Problem33.scala
@@ -0,0 +1,15 @@
+package org.ak.scala.nn_problems.p33
+
+/**
+ * @author antonk
+ * @since  10/26/14 - 6:33 AM
+ */
+object Problem33 {
+  //  P33 (*) Determine whether two positive integer numbers are coprime.
+  //    Two numbers are coprime if their greatest common divisor equals 1.
+  //  scala> 35.isCoprimeTo(64)
+  //  res0: Boolean = true
+
+
+  def isCoprime(a: Int, b: Int) = false
+}
