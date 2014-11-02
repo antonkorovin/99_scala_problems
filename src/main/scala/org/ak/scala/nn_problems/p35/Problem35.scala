361c0e1ef90816dbd24fd844e23cd0ae24307064
Anton Korovin <korovin.anton@gmail.com>
2014-11-02 13:39:10 +0200

    Added stub for Problem35.

---
+++ src/main/scala/org/ak/scala/nn_problems/p35/Problem35.scala
@@ -0,0 +1,14 @@
+package org.ak.scala.nn_problems.p35
+
+/**
+ * @author antonk
+ * @since  11/2/14 - 1:35 PM
+ */
+object Problem35 {
+  //  P35 (**) Determine the prime factors of a given positive integer.
+  //  Construct a flat list containing the prime factors in ascending order.
+  //    scala> 315.primeFactors
+  //  res0: List[Int] = List(3, 3, 5, 7)
+
+  def primeFactors(a: Int) = List.empty[Int]
+}
