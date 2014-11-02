87ee77bed1410b16079dc92ed31ce548643b4996
Anton Korovin <korovin.anton@gmail.com>
2014-11-02 13:39:30 +0200

    Added method `primeFactors` to S99Int.

--- src/main/scala/org/ak/scala/nn_problems/arithmetic/S99Int.scala
+++ src/main/scala/org/ak/scala/nn_problems/arithmetic/S99Int.scala
@@ -3,6 +3,7 @@ package org.ak.scala.nn_problems.arithmetic
 import org.ak.scala.nn_problems.p31.Problem31
 import org.ak.scala.nn_problems.p33.Problem33
 import org.ak.scala.nn_problems.p34.Problem34
+import org.ak.scala.nn_problems.p35.Problem35
 
 import scala.language.implicitConversions
 
@@ -14,6 +15,7 @@ class S99Int(start: Int) {
   def isPrime = Problem31.isPrime(start)
   def isCoprimeTo(b: Int) = Problem33.isCoprime(start, b)
   def totient = Problem34.totient(start)
+  def primeFactors = Problem35.primeFactors(start)
 }
 
 object S99Int {
