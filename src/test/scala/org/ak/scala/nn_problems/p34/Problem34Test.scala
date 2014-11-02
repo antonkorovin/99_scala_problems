ccddfc71a964102967038ccb522f374e696ca5d6
Anton Korovin <korovin.anton@gmail.com>
2014-11-02 13:30:50 +0200

    Added pending test for Problem34.

---
+++ src/test/scala/org/ak/scala/nn_problems/p34/Problem34Test.scala
@@ -0,0 +1,20 @@
+package org.ak.scala.nn_problems.p34
+
+import org.ak.scala.nn_problems.arithmetic.S99Int._
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+ * @author antonk
+ * @since  11/2/14 - 1:26 PM
+ */
+class Problem34Test
+  extends FunSuite
+          with Matchers {
+
+  test("example") {
+    pendingUntilFixed {
+      10.totient shouldEqual 4
+    }
+  }
+
+}
