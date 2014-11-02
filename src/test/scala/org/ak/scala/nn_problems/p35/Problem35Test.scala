fdccd60bb3e2aa8fffccbef43a01a231dedbf246
Anton Korovin <korovin.anton@gmail.com>
2014-11-02 13:39:46 +0200

    Added pending Problem35Test.

---
+++ src/test/scala/org/ak/scala/nn_problems/p35/Problem35Test.scala
@@ -0,0 +1,22 @@
+package org.ak.scala.nn_problems.p35
+
+import org.ak.scala.nn_problems.arithmetic.S99Int._
+import org.scalatest.{Matchers, FunSuite}
+
+/**
+ * @author antonk
+ * @since  11/2/14 - 1:36 PM
+ */
+class Problem35Test
+  extends FunSuite
+          with Matchers {
+
+  test("example") {
+    pendingUntilFixed{
+
+      315.primeFactors shouldEqual List(3, 3, 5 ,6)
+
+    }
+  }
+
+}
