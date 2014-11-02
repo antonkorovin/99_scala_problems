53e2aeca7eac0220fed795f369b30348ac4f2028
Anton Korovin <korovin.anton@gmail.com>
2014-11-02 13:29:43 +0200

    Added stub for Problem34.

---
+++ src/main/scala/org/ak/scala/nn_problems/p34/Problem34.scala
@@ -0,0 +1,14 @@
+package org.ak.scala.nn_problems.p34
+
+/**
+ * @author antonk
+ * @since  11/2/14 - 1:25 PM
+ */
+object Problem34 {
+  //  P34 (**) Calculate Euler's totient function phi(m).
+  //    Euler's so-called totient function phi(m) is defined as the number of positive integers r (1 <= r <= m) that are coprime to m.
+  //  scala> 10.totient
+  //  res0: Int = 4
+
+  def totient(a: Int) = 0
+}
