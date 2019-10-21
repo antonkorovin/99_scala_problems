54fdb0c703fbdbcbda968bdfca57207ecdff15d5
Anton Korovin <korovin.anton@gmail.com>
2019-10-21 17:40:56 +0300

    [p-40] Add property-based test.

--- src/test/scala/org/ak/scala/nn_problems/p40/Problem40Test.scala
+++ src/test/scala/org/ak/scala/nn_problems/p40/Problem40Test.scala
@@ -1,13 +1,35 @@
 package org.ak.scala.nn_problems.p40
 
+import org.ak.scala.nn_problems.arithmetic._
+import org.scalacheck.Gen
 import org.scalatest.{FunSuite, Matchers}
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
 class Problem40Test
   extends FunSuite
-    with Matchers {
+    with Matchers
+    with ScalaCheckDrivenPropertyChecks {
 
   test("smoke") {
     Problem40.goldbachConjecture(28) shouldBe (5 -> 23)
+  }
+
+
+  test("property-based check") {
+    val minValue = 2
+    val maxValue = 10
+
+    forAll(Gen.chooseNum(minValue, maxValue)) {
+      n =>
+        val evenValue = 2 * n
+
+        val (k, m) = Problem40.goldbachConjecture(evenValue)
+
+        k.isPrime shouldBe true
+        m.isPrime shouldBe true
+
+        k + m shouldBe evenValue
+    }
   }
 
 }
